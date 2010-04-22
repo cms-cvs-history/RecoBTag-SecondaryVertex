f6b933c70db8d3629186326b9d730a06a43cbf2a
cmsbuild <>
2010-04-22 07:08:15 +0000

    This commit was manufactured by cvs2git to create tag 'RecoBTag- SecondaryVertex-V01-04-02-02'.
    Sprout from master 2010-01-02 20:09:18 UTC David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch> 'libchecker driven dependency cleanup -round 2'
    Cherrypick from master 2010-04-22 07:08:14 UTC Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch> 'readd for backporting':
        interface/SimpleSecondaryVertexComputer.h
        python/simpleSecondaryVertex3TrkES_cfi.py
        python/simpleSecondaryVertexES_cfi.py
        python/simpleSecondaryVertexHighPurBJetTags_cfi.py

--- python/simpleSecondaryVertex3TrkES_cfi.py
+++ python/simpleSecondaryVertex3TrkES_cfi.py
@@ -1,10 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-simpleSecondaryVertex = cms.ESProducer("SimpleSecondaryVertexESProducer",
+simpleSecondaryVertex3Trk = cms.ESProducer("SimpleSecondaryVertexESProducer",
 	use3d = cms.bool(True),
 	unBoost = cms.bool(False),
 	useSignificance = cms.bool(True),
-	minTracks = cms.uint32(2)
+	minTracks = cms.uint32(3)
 )
-
-
