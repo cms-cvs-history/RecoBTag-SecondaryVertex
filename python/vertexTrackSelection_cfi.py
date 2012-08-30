a67851831104ee38e2301b592aeb577b22da396a
cmsbuild <>
2012-08-30 12:46:19 +0000

    This commit was manufactured by cvs2git to create tag 'RecoBTag- SecondaryVertex-doubleTagIVFPrototype_CMSSW533'.
    Sprout from master 2012-08-30 12:46:17 UTC Alexander Schmidt <sha1-46a3def9e8babfe312520fa12053fc84be309389@cern.ch> 'add IVF test'
    Cherrypick from master 2010-04-23 14:04:11 UTC Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch> 'bug fix. not affecting the standard sequence since filtering already done at IPTaginfo level':
        interface/TrackSelector.h
        python/trackPseudoSelection_cfi.py
        python/trackSelection_cfi.py
        python/vertexTrackSelection_cfi.py
        src/TrackSelector.cc

--- python/vertexTrackSelection_cfi.py
+++ python/vertexTrackSelection_cfi.py
@@ -1,10 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoBTag.ImpactParameter.variableJTA_cfi import *
-
 vertexTrackSelectionBlock = cms.PSet(
 	trackSelection = cms.PSet(
-                variableJTAPars,
 		totalHitsMin = cms.uint32(8),
 		jetDeltaRMax = cms.double(0.3),
 		qualityClass = cms.string('highPurity'),
@@ -20,7 +17,6 @@ vertexTrackSelectionBlock = cms.PSet(
 		sip3dValMax = cms.double(99999.9),
 		sip3dValMin = cms.double(-99999.9),
 		sip2dValMin = cms.double(-99999.9),
-		normChi2Max = cms.double(99999.9),
-                useVariableJTA = cms.bool(False) 
+		normChi2Max = cms.double(99999.9)
 	)
 )
